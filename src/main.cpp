7e331f426de13f1a98f2682ebb229742e01ba181
Kinupo <Kinupo@soj-interstellar.space>
2021-08-24 20:35:15 -0500

    moved window management to window repository.  Added lazy initilization of windows

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,9 @@ std::vector<std::unique_ptr<Row>> LoadProfileList(){
 int main() {
 
   bool should_quit = false;
-  auto ncurses_engine = NcursesEngineConfiguration::GenerateConectionTestEngine();
+  
+  auto window_repository = NcursesEngineConfiguration::GenerateWindowRepository();
+  auto ncurses_engine = NcursesEngineConfiguration::GenerateConectionTestEngine(*window_repository);
 
   ncurses_engine->Draw();
   while (!should_quit) {
@@ -25,19 +27,19 @@ int main() {
     ncurses_engine->Draw();
     ncurses_engine->ProcessInput();
 
-    auto window = ncurses_engine->WindowByName("menue");
+    auto window = window_repository->WindowByName("menue");
     auto menu_window = std::dynamic_pointer_cast<MenuWindow>(window);
     auto menu_selection = menu_window->Selected();
     if(menu_selection == "Quit")
       should_quit = true;
     else if (menu_selection == "Load"){
-      auto window = ncurses_engine->WindowByName("File Selector");
+      auto window = window_repository->WindowByName("File Selector");
       auto profile_window = std::dynamic_pointer_cast<TableWindow>(window);
       profile_window->Show();
       auto rows = LoadProfileList();
       profile_window->TableData(std::move(rows));
 
-      ncurses_engine->WindowByName("Connection Table")->Hide();
+      window_repository->WindowByName("Connection Table")->Hide();
     }
 
     //handle menu selection
