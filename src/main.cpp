b90eca23d256d837a80d2219b083501a0d039bae
Kinupo <Kinupo@soj-interstellar.space>
2021-08-25 14:08:08 -0500

    Initial profiles list loader.  Updated WindowByName to use templates to get a specific subtype.

--- src/main.cpp
+++ src/main.cpp
@@ -2,41 +2,37 @@
 #include <string>
 
 #include "configuration/ncurses_engine_config.h"
+#include "profile/profile_loader.h"
 
 void LoadProfile(){}
 void RunConnetionTest(){}
 
-std::vector<std::unique_ptr<Row>> LoadProfileList(){
-  auto rows = std::vector<std::unique_ptr<Row>>();
-  auto row = std::unique_ptr<Row>(new Row());
-  row->data.emplace_back("Profile 1");
-  rows.emplace_back(std::move(row));
-  return rows;
+
+std::string ReadMenuSelection(WindowRepository& window_repository){
+    auto menu_window = window_repository.WindowByName<MenuWindow>("menue");
+    return menu_window->Selected();
 }
 
 int main() {
 
   bool should_quit = false;
-  
+
   auto window_repository = NcursesEngineConfiguration::GenerateWindowRepository();
   auto ncurses_engine = NcursesEngineConfiguration::GenerateConectionTestEngine(*window_repository);
 
-  ncurses_engine->Draw();
   while (!should_quit) {
     
     ncurses_engine->Draw();
     ncurses_engine->ProcessInput();
 
-    auto window = window_repository->WindowByName("menue");
-    auto menu_window = std::dynamic_pointer_cast<MenuWindow>(window);
-    auto menu_selection = menu_window->Selected();
+    auto menu_selection = ReadMenuSelection(*window_repository);
     if(menu_selection == "Quit")
       should_quit = true;
     else if (menu_selection == "Load"){
-      auto window = window_repository->WindowByName("File Selector");
-      auto profile_window = std::dynamic_pointer_cast<TableWindow>(window);
+      auto profile_window = window_repository->WindowByName<TableWindow>("File Selector");
+      // auto profile_window = std::dynamic_pointer_cast<TableWindow>(window);
       profile_window->Show();
-      auto rows = LoadProfileList();
+      auto rows = ProfileLoader::LoadProfileList();
       profile_window->TableData(std::move(rows));
 
       window_repository->WindowByName("Connection Table")->Hide();
@@ -44,6 +40,10 @@ int main() {
 
     //handle menu selection
     //handle profile load
+    //event store
+    //event dispatcher
+    //protocols
+    //connections
   }
 
   return 0;
