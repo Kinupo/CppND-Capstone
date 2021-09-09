140b679dfbdd24fc8b360cef52b1f8b1180c3228
Kinupo <Kinupo@soj-interstellar.space>
2021-09-08 19:20:38 -0500

    added check to see if a window processed a click

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,12 @@
 #include "configuration/windows_configuration.h"
 #include "connection_tester/connection_tester.h"
 
-void RunConnetionTest(){}
+void FileSelectorHandler(WindowRepository &window_repository, ConnectionTester &connection_tester){
 
-bool HandleInput(WindowRepository &window_repository, ConnectionTester &connection_tester){
-    auto menu_selection = window_repository.WindowByName<MenuWindow>("menue")->Selected();
-    if(menu_selection == "Quit")
-      return true;
-    else if (menu_selection == "Load"){
-      //Load Profiles into the profile selction window
-      auto profile_window = window_repository.WindowByName<TableWindow>("File Selector");
-      profile_window->TableData(std::move(ProfileLoader::LoadProfileList()));
-
-      //display profile loader, hide connection table (they share the same screen space)
-      profile_window->Show();
-      window_repository.WindowByName("Connection Table")->Hide();
-    } else if(window_repository.WindowByName("File Selector")->IsVisable()){
       auto table_selection = window_repository.WindowByName<TableWindow>("File Selector")->Selected();
-      //  auto notification_window = window_repository->WindowByName<NotificationWindow>("Notification");
-      // notification_window->DisplayMessage("Not selected");
       if(table_selection.size() != 0){
+
         auto connection_profiles = ProfileLoader::LoadProfile(table_selection.at(0));
-        // notification_window->DisplayMessage();
         window_repository.WindowByName("File Selector")->Hide();
         window_repository.WindowByName("Connection Table")->Show();
 
@@ -39,9 +24,11 @@ bool HandleInput(WindowRepository &window_repository, ConnectionTester &connecti
         
         connection_tester.TestConnections(std::move(connections));
       }
-      
-    } else if(window_repository.WindowByName("Connection Table")->IsVisable()){
-      
+}
+
+std::vector<std::unique_ptr<std::vector<std::string>>> FindUpdatedTestStatus(
+  ConnectionTester &connection_tester){
+  
       auto connection_test_status = connection_tester.Status();
       std::vector<std::unique_ptr<std::vector<std::string>>> connection_table_data;
       for(auto& status:*connection_test_status){
@@ -52,14 +39,35 @@ bool HandleInput(WindowRepository &window_repository, ConnectionTester &connecti
         status_row->emplace_back(std::to_string(status->duration_ms) + " ms");
         connection_table_data.emplace_back(std::move(status_row));
       }
+      return connection_table_data;
+}
 
-      window_repository.WindowByName<NotificationWindow>("Notification")->DisplayMessage("Number of results" + std::to_string(connection_table_data.size()));
+bool HandleInput(WindowRepository &window_repository, ConnectionTester &connection_tester){
 
+    auto menu_selection = window_repository.WindowByName<MenuWindow>("menue")->Selected();
+    if(menu_selection == "Quit")
+      return true;
+    else if (menu_selection == "Load"){
+      //Load Profiles into the profile selction window
+      auto profile_window = window_repository.WindowByName<TableWindow>("File Selector");
+      profile_window->TableData(std::move(ProfileLoader::LoadProfileList()));
+
+      //display profile loader, hide connection table (they share the same screen space)
+      profile_window->Show();
+      window_repository.WindowByName("Connection Table")->Hide();
+    }
+
+    if(window_repository.WindowByName("File Selector")->IsVisable()){
+      FileSelectorHandler(window_repository, connection_tester);
+    } 
+    //needs to be after File Selector handler so the connection table will update as soon
+    //as the test starts
+    if(window_repository.WindowByName("Connection Table")->IsVisable()){
+      auto connection_table_data = FindUpdatedTestStatus(connection_tester);
       window_repository.WindowByName<TableWindow>("Connection Table")->TableData(std::move(connection_table_data));
-
     }
     
-      return false;
+    return false;
 }
 
 int main() {
@@ -74,14 +82,14 @@ int main() {
     *window_repository, 
     window_names);
   auto connection_tester = std::make_unique<ConnectionTester>();
-
+  int loop_count = 0;
   while (!should_quit) {
     
     ncurses_engine->Draw();
     ncurses_engine->ProcessInput();
 
     should_quit = HandleInput(*window_repository, *connection_tester);
- 
+    window_repository->WindowByName<NotificationWindow>("Notification")->DisplayMessage(std::to_string(loop_count++));
     //event store
     //event dispatcher
     //protocols
