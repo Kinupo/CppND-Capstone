18aaf2238c9ea0e31afd36a542ab132270f0ab94
Kinupo <Kinupo@soj-interstellar.space>
2021-09-10 01:37:52 -0500

    Added future to connect test.  It also fixed the delay on unsuported protocol results

--- src/connection_tester/protocols/tcp.cpp
+++ src/connection_tester/protocols/tcp.cpp
@@ -1,4 +1,5 @@
 #include "connection_tester/protocols/tcp.h"
+#include <future>
 
 Tcp::Tcp() : Protocol("TCP"){};
 
@@ -6,12 +7,12 @@ void ConnectToHost(
     const boost::asio::ip::basic_resolver_results<boost::asio::ip::tcp>& resolved_addresses,
     std::shared_ptr<boost::beast::tcp_stream> stream,
     std::shared_ptr<std::condition_variable> connect_condition,
-    bool& timed_out){
+    std::promise<bool> timed_out_promise){
 
     if(resolved_addresses.end() != resolved_addresses){
         stream->connect(resolved_addresses);  
     }
-    timed_out = false;
+    timed_out_promise.set_value(false);
     connect_condition->notify_all();
 }
 
@@ -31,14 +32,20 @@ std::string Tcp::TestConnection(std::string address){
     auto stream = std::make_shared<boost::beast::tcp_stream>(io_context);
 
     try {
-        bool timed_out = true;
+        std::promise<bool> timed_out_promise;
+        auto timed_out_future = timed_out_promise.get_future();
+
         auto const resolved_addresses = address_resolver.resolve(host, port);
-        std::thread connection_thread(ConnectToHost, resolved_addresses, stream, connect_condition, std::ref(timed_out));
+        std::thread connection_thread(ConnectToHost, resolved_addresses, stream, connect_condition, std::move(timed_out_promise));
         connection_thread.detach();
+        
         connect_condition->wait_for(connect_lock, timeout);
         stream->close();
-        if(timed_out)
+        
+        if(timed_out_future.valid() 
+            && timed_out_future.wait_for(std::chrono::seconds(1)) == std::future_status::timeout)
             return "Timed Out";
+
     } catch (const boost::system::system_error& exception){
         return exception.what();
     }
