3be20862204dad3a87da54602d68d5ea79f65151
Kinupo <Kinupo@soj-interstellar.space>
2021-09-09 19:57:49 -0500

    Renamed httphead to TCP

--- include/configuration/connection_tester_configuration.h
+++ include/configuration/connection_tester_configuration.h
@@ -2,13 +2,13 @@
 #define _CONFIGURATION_CONNECTION_TESTER_CONFIGURATION_H_
 
 #include "connection_tester/connection_tester.h"
-#include "connection_tester/protocols/http_head.h"
+#include "connection_tester/protocols/tcp.h"
 
 namespace ConnectionTesterConfiguration {
 
     std::unique_ptr<ConnectionTester> GenerateConnectionTester(){
         auto connection_tester = std::make_unique<ConnectionTester>();
-        connection_tester->RegisterProtocol(std::make_shared<HttpHead>());
+        connection_tester->RegisterProtocol(std::make_shared<Tcp>());
         return connection_tester;
     }
       
